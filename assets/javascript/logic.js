7b278282015f1efd77038240beb7ffdd18c43dd1
Stephen Madison <stpehen.d.madison@gmail.com>
2019-07-02 14:21:28 -0500

    updated html, css, and sounds

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,145 +1,135 @@
 
-        var words = ["rabbit", "snail", "airport", "carrots", "river"];
-        var wins = 0;
-        var losses = 0;
-        var wrongLetter = [];
-        var guessesLeft = 10;
-        var underScores = [];
-        var userGuesses = [];
-        var ranWord;
-        var winCounter = 0;
-        var letters = [];
-
-        function startGame() {
-
-            ranWord = words[Math.floor(Math.random() * words.length)];
-
-            console.log('Random Word: ' + ranWord);
-
-            for (var i = 0; i < ranWord.length; i++) {
-
-                underScores.push('_');
-
-            }
-            // Printing Underscores
-
-            document.getElementById('answerText').innerHTML = underScores.join(" ");
-
-            letters = [ranWord.length];
-
-            //reset
-            wrongLetter = [];
-            guessesLeft = 10;
-            wins = 0;
-            losses = 0;
-
-            document.getElementById('guesses-left').textContent = guessesLeft;
-            document.getElementById('wins-text').textContent = wins;
-            document.getElementById('losses-text').textContent = losses;
-            document.getElementById('incorrect-guesses').textContent = wrongLetter;
+var words = ["atoms", "homer", "marge", "lisa", "bart", "maggie", "ned", "smithers", "burns", "bumblebee", "krusty", "barney", "moe", "carl", "lenny", "shelbyville", "monorail", "sideshow", "apu", "otto", "skinner", "ralph", "grampa", "santa", "patty", "selma", "troy", "herbert", "helen", "nelson", "krustofsky", "rod"];
+var wins = 0;
+var losses = 0;
+var wrongLetter = [];
+var guessesLeft = 10;
+var underScores = [];
+var userGuesses = [];
+var ranWord;
+var letters = [];
 
 
-        }
 
-        function winLose() {
+function startGame() {
 
-            var guessedWord = letters.join("");
+    underScores = [];
 
-            if (guessedWord === ranWord) {
-                
-                wins++;
-                document.getElementById('wins-text').innerHTML = wins;
-                alert("Winner");
-                softReset()
+    ranWord = words[Math.floor(Math.random() * words.length)];
 
-            }
+    console.log('Random Word: ' + ranWord);
 
-            else if (guessesLeft === 0) {
+    for (var i = 0; i < ranWord.length; i++) {
 
-                alert("Loser");
-                losses++;
-                document.getElementById('losses-text').innerHTML = losses;
-                softReset()
+        underScores.push('_');
 
-            }
+    }
+    // Printing Underscores
 
+    document.getElementById('answerText').innerHTML = underScores.join(" ");
 
-        }
+    letters = [ranWord.length];
 
-        // User Guesses
+    //reset
+    wrongLetter = [];
+    guessesLeft = 10;
+    wins = 0;
+    losses = 0;
 
-        document.onkeyup = function (event) {
-
-            userGuesses = event.key;
-
-            //Checking if the letter exist inside of the word
-
-            if (ranWord.indexOf(userGuesses) > -1) {
-
-                for (var i = 0; i < ranWord.length; i++) {
-
-                    if (ranWord[i] === userGuesses) {
-                        letters[i] = userGuesses;
-                        underScores[i] = userGuesses;
-                        console.log(underScores);
-                        document.getElementById('answerText').innerHTML = underScores.join(" ");
-                        winCounter++;
-                        winLose();
+    document.getElementById("theme").play();
+    document.getElementById('guesses-left').textContent = guessesLeft;
+    document.getElementById('wins-text').textContent = wins;
+    document.getElementById('losses-text').textContent = losses;
+    document.getElementById('incorrect-guesses').textContent = wrongLetter;
 
 
-                    }
+}
 
 
-                }
-            }
-            else {
+// Wins and Loses Calculations
 
-                wrongLetter.push(userGuesses);
-                guessesLeft--;
-                document.getElementById('guesses-left').innerHTML = guessesLeft;
-                document.getElementById('incorrect-guesses').textContent = wrongLetter;
+function winLose() {
+
+    var guessedWord = letters.join("");
+
+    if (guessedWord === ranWord) {
+
+        wins++;
+        document.getElementById('wins-text').innerHTML = wins;
+        document.getElementById("aye").play();
+        alert("Winner");
+        softReset()
+
+    }
+
+    else if (guessesLeft === 0) {
+       
+        alert("Loser");
+        losses++;
+        document.getElementById('losses-text').innerHTML = losses;
+        softReset()
+    }
+
+
+}
+
+// User Guesses
+
+document.onkeyup = function (event) {
+
+    userGuesses = event.key;
+
+    //Checking if the letter exist inside of the word
+
+    if (ranWord.indexOf(userGuesses) > -1) {
+
+        for (var i = 0; i < ranWord.length; i++) {
+
+            if (ranWord[i] === userGuesses) {
+                letters[i] = userGuesses;
+                underScores[i] = userGuesses;
+                console.log(underScores);
+                document.getElementById('answerText').innerHTML = underScores.join(" ");
+                document.getElementById("woo").play();
                 winLose();
-
             }
-        }
-
-        function reset() {
-            letters = [ranWord.length];
-            wrongLetter = [];
-            guessesLeft = 10;
-            wins = 0;
-            losses = 0;
-            for (var i = 0; i < ranWord.length; i++) {
-
-                underScores.push('_');
-
-            }
-            // Printing Underscores
-
-            document.getElementById('answerText').innerHTML = underScores.join(" ");
-
-        }
-
-        function softReset() {
-            letters = [ranWord.length];
-            ranWord = words[Math.floor(Math.random() * words.length)];
-            wrongLetter = [];
-            guessesLeft = 10;
-            document.getElementById('guesses-left').textContent = guessesLeft;
-            console.log('Random Word: ' + ranWord);
-            underScores = [];
-            for (var i = 0; i < ranWord.length; i++) {
-                underScores.push('_');
-
-            }
-            // Printing Underscores
-            
-            document.getElementById('incorrect-guesses').textContent = wrongLetter;
-            document.getElementById('answerText').innerHTML = underScores.join(" ");
 
 
         }
+    }
+    else {
+        document.getElementById("doh").play();
+        wrongLetter.push(userGuesses);
+        guessesLeft--;
+        document.getElementById('guesses-left').innerHTML = guessesLeft;
+        document.getElementById('incorrect-guesses').textContent = wrongLetter; 
+        winLose();
+    
+    }
+}
+
+// Reset function that doesnt reset the score
+
+function softReset() {
+    letters = [ranWord.length];
+    ranWord = words[Math.floor(Math.random() * words.length)];
+    wrongLetter = [];
+    guessesLeft = 10;
+    document.getElementById('guesses-left').textContent = guessesLeft;
+    console.log('Random Word: ' + ranWord);
+    underScores = [];
+    for (var i = 0; i < ranWord.length; i++) {
+        underScores.push('_');
+
+    }
+    // Printing Underscores
+
+    document.getElementById('incorrect-guesses').textContent = wrongLetter;
+    document.getElementById('answerText').innerHTML = underScores.join(" ");
+
+
+}
 
 
 
-        startGame();
+startGame();
